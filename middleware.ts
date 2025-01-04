b15df56b4929f459648ab57ddea8a9714195db5f
unknown <shababhasnat@gmail.com>
2025-01-04 15:56:02 -0600

    test middleware imnplemented to create simple subdomains

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
 
@@ -6,6 +7,31 @@ export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
     await auth.protect()
   }
+
+  const url = request.nextUrl
+  const pathname = url.pathname
+
+  const hostname = request.headers.get('host')
+
+  let currentHost
+
+  if (process.env.NODE_ENV === 'production') {
+    const baseDomain = process.env.BASE_DOMAIN
+    currentHost = hostname?.replace(`.${baseDomain}`, '')
+  } else {
+    currentHost = hostname?.split(':')[0].replace('.localhost', '')
+
+  }
+
+  if (!currentHost) {
+    return NextResponse.next()
+  }
+
+  // testing subdomain
+  return NextResponse.rewrite(new URL(`/tenant${pathname}`, request.url))
+
+  
+
 })
 
 export const config = {
