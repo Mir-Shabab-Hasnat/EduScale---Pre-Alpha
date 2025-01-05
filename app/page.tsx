701be0b4ce83ada37b207029a88e49e8ed4b70ce
unknown <shababhasnat@gmail.com>
2025-01-05 16:42:16 -0600

    added magicui

--- app/page.tsx
+++ app/page.tsx
@@ -4,46 +4,55 @@ import { Navbar } from "../components/navbar"
 import { Hero } from "../components/Hero";
 import { Footer } from "../components/Footer";
 import { useEffect, useState } from "react";
+import PageWrapper from "@/components/new/container/page-wrapper";
 
 type Theme = "light" | "dark" | "system";
 
 
 export default function Home() {
-  const [theme, setTheme] = useState<Theme>("system");
-  useEffect(() => {
-    const systemPreference = window.matchMedia("(prefers-color-scheme: dark)")
-      .matches
-      ? "dark"
-      : "light";
-    const savedTheme = (localStorage.getItem("theme") as Theme) || "system";
-    setTheme(savedTheme);
-    const themeToApply =
-      savedTheme === "system" ? systemPreference : savedTheme;
-    document.documentElement.classList.toggle("dark", themeToApply === "dark");
-  }, []);
-  const handleThemeChange = (newTheme: Theme) => {
-    setTheme(newTheme);
-    localStorage.setItem("theme", newTheme);
-    if (newTheme === "system") {
-      const systemPreference = window.matchMedia("(prefers-color-scheme: dark)")
-        .matches
-        ? "dark"
-        : "light";
-      document.documentElement.classList.toggle(
-        "dark",
-        systemPreference === "dark",
-      );
-    } else {
-      document.documentElement.classList.toggle("dark", newTheme === "dark");
-    }
-  };
   return (
-    <main className="min-h-screen bg-black text-white">
-      <Navbar />
-      <Hero />
-      <Footer theme={theme} setTheme={handleThemeChange}/>
-    </main>
+    <PageWrapper>
+      <div className="flex flex-col max-w-[70rem]">
+        <HeroSection />
+      </div>
+    </PageWrapper>
   );
+
+  // const [theme, setTheme] = useState<Theme>("system");
+  // useEffect(() => {
+  //   const systemPreference = window.matchMedia("(prefers-color-scheme: dark)")
+  //     .matches
+  //     ? "dark"
+  //     : "light";
+  //   const savedTheme = (localStorage.getItem("theme") as Theme) || "system";
+  //   setTheme(savedTheme);
+  //   const themeToApply =
+  //     savedTheme === "system" ? systemPreference : savedTheme;
+  //   document.documentElement.classList.toggle("dark", themeToApply === "dark");
+  // }, []);
+  // const handleThemeChange = (newTheme: Theme) => {
+  //   setTheme(newTheme);
+  //   localStorage.setItem("theme", newTheme);
+  //   if (newTheme === "system") {
+  //     const systemPreference = window.matchMedia("(prefers-color-scheme: dark)")
+  //       .matches
+  //       ? "dark"
+  //       : "light";
+  //     document.documentElement.classList.toggle(
+  //       "dark",
+  //       systemPreference === "dark",
+  //     );
+  //   } else {
+  //     document.documentElement.classList.toggle("dark", newTheme === "dark");
+  //   }
+  // };
+  // return (
+  //   <main className="min-h-screen bg-black text-white">
+  //     <Navbar />
+  //     <Hero />
+  //     <Footer theme={theme} setTheme={handleThemeChange}/>
+  //   </main>
+  // );
 }
 
 
