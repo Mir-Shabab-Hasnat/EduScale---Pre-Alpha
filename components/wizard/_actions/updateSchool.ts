6bf736c997b77073c1c68e505e261423363bb042
Mir-Shabab-Hasnat <shababhasnat@gmail.com>
2025-01-13 23:36:52 -0600

    created simple wizard form schema and getting user info api

--- components/wizard/_actions/updateSchool.ts
+++ components/wizard/_actions/updateSchool.ts
@@ -1,7 +1,32 @@
 "use server"
 
-import React from 'react'
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export async function updateSchool() {
-    
+export async function CreateUserSchool({ schoolName }: {schoolName: string}) {
+    const user = await currentUser();
+    if (!user) {
+        redirect('/sign-in')
+    }
+
+    const existingSchool = await prisma.school.findUnique({
+        where: {
+            schoolId: user.id
+        }
+    })
+
+    if (existingSchool) {
+        throw new Error("School already exists")
+    }
+
+    const school = await prisma.school.create({
+        data: {
+            schoolId: user.id,
+            name: schoolName
+        }
+    })
+
+    return school
+
 }
